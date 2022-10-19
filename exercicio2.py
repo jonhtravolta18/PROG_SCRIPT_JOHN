ffc161f8da40bf8074a541adc1a451129b1af074
jonhtravolta18 <115498624+jonhtravolta18@users.noreply.github.com>
2022-10-19 12:06:16 -0300

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,20 @@
+#######################################################################
+# NOME = JOÃO ALEXANDRE A. DE SA
+# MATRICULA = 12211EAU020
+# TRABALHO = ps-01-aritmetica
+#######################################################################
+
+# DECLAREI QUE AS VARIAVEIS A SEGUIR VÃO RECEBER UM VALOR INTEIRO DIGITADO PELO USUÁRIO
+
+a = int(input())
+b = int(input())
+
+# PRINTEI ALGUMAS STRINTS CONCATENADAS COM O VALOR DAS VARIAVEIS E DEPOIS COM CALCULOS MATEMATICOS
+
+print("a =", a)
+print("b =", b)
+print("a + b =", a+b)
+print("a - b =", a-b)
+print("a * b =", a*b)
+print("a // b =", a//b)
+print("a % b =", a % b)
