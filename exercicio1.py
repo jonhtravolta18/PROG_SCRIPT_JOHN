22bcce9372c0717e80226abc9e3a983ae989efbf
jonhtravolta18 <115498624+jonhtravolta18@users.noreply.github.com>
2022-10-19 12:04:37 -0300

    Create exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,17 @@
+#######################################################################
+# NOME = JOÃO ALEXANDRE A. DE SA
+# MATRICULA = 12211EAU020
+# TRABALHO = 00-warmup
+#######################################################################
+
+
+# ESTOU DEFININDO QUE AS VARIÁVEIS "a" e "b", INDIVIDUALMENTE, RECEBERÃO
+# UM VALOR INTEIRO DIGITADO PELO USUÁRIO
+a = int(input())
+b = int(input())
+
+# DEFINI QUE VARIÁVEL "soma" TERÁ COMO VALOR A SOMA MATEMÁTICA DO VALOR INTEIRO DE "a" + "b".
+soma = a+b
+
+# ESTAREI PRINTANDO O TEXTO "X = " CONCATENADO COM O VALOR DA VARIAVEL SOMA
+print("X =", soma)
