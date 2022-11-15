616d3c79484da73e9d7d4c7b97490e431cdd1fbc
jonhtravolta18 <115498624+jonhtravolta18@users.noreply.github.com>
2022-11-15 16:45:40 -0300

    Update exercicio6.py

--- exercicio6.py
+++ exercicio6.py
@@ -1,44 +1,55 @@
-#######################################################################
+#############################################################################################################
 # NOME = JOÃO ALEXANDRE A. DE SA
 # MATRICULA = 12211EAU020
 # TRABALHO = ps-05-super-sete
-#######################################################################
+#############################################################################################################
+#####################- DEFININDO ALGUMAS FUNÇÕES AUXILIARES -################################################
 
-def print_home(a, b, c, d):#função para printar valores e strings que não vão se alterar
-	print(f"Primeira: {a}")
-	print(f"Terceira: {b}")
-	print(f"Quarta: {c}")
-	print(f"Sexta: {d}")
+def PRINT_HOME(A, B, C, D): #print das strings necessárias para o começo do programa.
+	print(f"Primeira: {A}") 
+	print(f"Terceira: {B}")
+	print(f"Quarta: {C}")
+	print(f"Sexta: {D}")
 	print(f"Lista de apostas:")
-
-def verify_condition(a): #função especifica para verificar se determinado valor da variavel é multiplo de 7 ou 13, retornando False caso seja multiplo de pelo menos um deles
-    if a%7!=0 and a%13!=0:
-    	return True
+#############################################################################################################
+def VERIFY_CONDITION(A): #verifica se a variável A é multipla de 7 e/ou 13, retornando True apenas se não for.
+    if (A%7!=0 and A%13!=0):
+        return True
     else:
-    	return False
-    
+        return False
+#############################################################################################################
+def REQUEST_INPUT(): #retorna um input de valor inteiro.
+    ENTRADA = int(input())
+    return ENTRADA
+#############################################################################################################
+def INCREMENTO(A): #incrementa em 1 o valor da variavel A.
+    A += 1
+    return A
+#####################- DEFININDO FUNÇÃO PRINCIPAL -##########################################################    
 def main():
-    primeiro = int(input())
-    terceiro = int(input())
-    quarto = int(input())
-    sexto = int(input())
-    
-    print_home(primeiro, terceiro, quarto, sexto)
-#nesses laços, eu defini que ele começasse a modificar os valores da direita pra esquerda, testando todas as possibilidades e printando caso a condicão seja atendida. Quando os valores deles ficassem iguais ao valor seguinte, eles iriam pro while anterior, até ter testado todas as possibilidades. 
-    i = 0
-    while ((primeiro+i)<=terceiro):
-        segundo = primeiro+i
-        j = 0
-        while ((quarto+j)<=sexto):
-            quinto = quarto+j
-            k = 0
-            while ((sexto+k)<=9):
-                setimo = sexto+k
-                k+=1
-                soma = primeiro+segundo+terceiro+quarto+quinto+sexto+setimo
-                if (verify_condition(soma)):
-                    print(f"{primeiro} - {segundo} - {terceiro} - {quarto} - {quinto} - {sexto} - {setimo}") 
-            j+=1
-        i+=1
-
+    #atribuo os valores digitados pelo usuário em sua variavel correspondente.
+    PRIMEIRO = REQUEST_INPUT(); TERCEIRO = REQUEST_INPUT(); QUARTO = REQUEST_INPUT(); SEXTO = REQUEST_INPUT()
+    #solicito a função para printar todas as strings do começo.
+    PRINT_HOME(PRIMEIRO, TERCEIRO, QUARTO, SEXTO)
+    #atribuo o valor inicial da variavel responsavel pelo incremento no primeiro laço.
+    I = 0
+    #ENQUANTO A SOMA DO "PRIMEIRO" COM O INCREMENTO "I" FOR MENOR QUE O "TERCEIRO".
+    while ((PRIMEIRO+I) <= (TERCEIRO)): 
+        SEGUNDO = (PRIMEIRO + I)
+        J = 0
+        #ENQUANTO A SOMA DO "QUARTO" COM O INCREMENTO "J" FOR MENOR QUE O "SEXTO".
+        while ((QUARTO+J) <= (SEXTO)):
+            QUINTO = (QUARTO + J)
+            K = 0
+            #ENQUANTO A SOMA DO "SEXTO" COM O INCREMENTO "K" FOR MENOR QUE "9".
+            while ((SEXTO+K) <= (9)):
+                SETIMO = (SEXTO + K)
+                K = INCREMENTO(K)
+                #CHAMA A FUNÇÃO QUE IRA VERIFICAR SE O RESULTADO DA SOMA ATENDE A CONDIÇÃO DO PROBLEMA.
+                SOMA = (PRIMEIRO + SEGUNDO + TERCEIRO + QUARTO + QUINTO + SEXTO + SETIMO)
+                if (VERIFY_CONDITION(SOMA)):
+                    print(f"{PRIMEIRO} - {SEGUNDO} - {TERCEIRO} - {QUARTO} - {QUINTO} - {SEXTO} - {SETIMO}") 
+            J = INCREMENTO(J)
+        I = INCREMENTO(I)
+#############################################################################################################
 main()
