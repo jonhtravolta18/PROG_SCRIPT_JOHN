cca42193082f33947835aa2e06ac7c8ce06cb0b4
jonhtravolta18 <115498624+jonhtravolta18@users.noreply.github.com>
2022-11-22 22:41:55 -0300

    Update exercicio5.py

--- exercicio5.py
+++ exercicio5.py
@@ -3,66 +3,76 @@
 # MATRICULA = 12211EAU020
 # TRABALHO = ps-04-street-figther
 #######################################################################
-
-#criando 2 funções de acordo com a vitoria de cada lutador, Ken ou Ryu.
-def vitoria_ken(g_ryu, g_ken): 
+def KenVitory(RyuG, KenG): #criando função para quando a vitória for do KEN
     print("LUTADOR VENCEDOR: KEN")  #nome do vencedor
-    print(f"GOLPES RYU = {g_ryu}") #qntd de golpes do ryu
-    print(f"GOLPES KEN = {g_ken}") #qntd de golpes do ken
-def vitoria_ryu(g_ryu, g_ken):
-    print("LUTADOR VENCEDOR: RYU")
-    print(f"GOLPES RYU = {g_ryu}") 
-    print(f"GOLPES KEN = {g_ken}")
+    print(f"GOLPES RYU = {RyuG}") #qntd de golpes do ryu
+    print(f"GOLPES KEN = {KenG}") #qntd de golpes do ken
 
-#criando função principal  
-def main(): 
+def RyuVitory(RyuG, KenG): #criando função para quando a vitória for do Ryu
+    print("LUTADOR VENCEDOR: RYU") #nome do vencedor
+    print(f"GOLPES RYU = {RyuG}") #qntd de golpes do ryu
+    print(f"GOLPES KEN = {KenG}") #qntd de golpes do ken
+
+def SubtraiXdeY(y, x): #criando função para subtrair um valor x de outro y
+    if x<0: #porém, se x for menor que 0, eu quero que ele se torne positivo antes dessa subtração
+        x *= -1
+    y -= x
+    return y #depois de tudo, retorno o valor de y
+
+def VerifyLifeNeg(HP_X): #criando uma função especifica para o código, que verifica se o parametro é menor que zero
+    if HP_X<0:
+        return True #retornado TRUE caso seja negativo
+    elif HP_X>=0:
+        return False #e FALSE caso seja positivo
+
+def PrintHPS(RyuLife, KenLife): #outra função especifica focada somente em printar o HP dos lutadores
+    print(f"HP RYU = {RyuLife}")
+    print(f"HP KEN = {KenLife}")
+
+def Incremento(x): #função genérica construida para incrementar em 1 o parametro fornecido e depois retornar ele
+    x += 1
+    return x
+
+def main(): #criando função principal
+    #informando quantidade inicial de golpes
+    GOLPES_RYU = 0 
+    GOLPES_KEN = 0 
     #input do hp do lutador 
     HP_RYU = int(input()) 
     HP_KEN = int(input())
-    #######################
 
-    ########################################
-    #informando quantidade inicial de golpes
-    GOLPES_RYU = 0 
-    GOLPES_KEN = 0
-    ########################################
-    
     while (HP_RYU>0 and HP_KEN>0): #criando laço de acordo com o hp dos lutadores
         VALOR_ATAQUE = int(input()) #input do valor de ataque (repete até a condição do while ser aceita)
         if VALOR_ATAQUE>0:
-            #nesse IF, caso o ataque tenha valor maior que 0, o ryu atacará, realizando um print da na tela
-            print(f"RYU APLICOU UM GOLPE: {(VALOR_ATAQUE*-1)}")
-            #diminuindo a vida do ken a partir da subtração do valor absoluto do ataque
-            HP_KEN -= (VALOR_ATAQUE*-1)
-            #após isso já verifico se o hp vai ficar um valor negativo, alterando-o, caso sim, para 0.
-            if HP_KEN<=0:
+    #nesse IF, caso o ataque tenha valor maior que 0, o ryu atacará, realizando um print da na tela
+            print(f"RYU APLICOU UM GOLPE: {VALOR_ATAQUE}")
+    #chamo a função e o retorno dela vai ser atribuido a vida do KEN
+            HP_KEN = SubtraiXdeY(HP_KEN, VALOR_ATAQUE)
+    #chamo a função que retornará TRUE caso o hp seja negativo e depois atribuo 0 ao hp do ken, outrem, nada acontece
+            if VerifyLifeNeg(HP_KEN):
                 HP_KEN=0
-            #termino printando o valor do hp de ambos os jogadores e adicionando 1 ao número de golpes
-            #realizados pelo ryu
-            print(f"HP RYU = {HP_RYU}")
-            print(f"HP KEN = {HP_KEN}")
-            GOLPES_RYU += 1
+    #chamo a função que irá printar o hp dos lutadores
+            PrintHPS(HP_RYU, HP_KEN)
+    #chamo a função que irá incrementar o valor dos golpes de ryu e já atribuir à variavel golpes
+            GOLPES_RYU = Incremento(GOLPES_RYU)
         
         elif VALOR_ATAQUE<0:
-            #nesse ELIF, caso o ataque tenha valor menor que 0, o ken atacará, realizando um print da na tela
+    #nesse ELIF, caso o ataque tenha valor menor que 0, o ken atacará, realizando um print da na tela
             print(f"KEN APLICOU UM GOLPE: {(VALOR_ATAQUE*-1)}")
-            #diminuindo a vida do ryu a partir da subtração do valor absoluto do ataque
-            HP_RYU -= (VALOR_ATAQUE*-1)
-            if HP_RYU<=0:
+    #chamo a função e o retorno dela vai ser atribuido a vida do RYU
+            HP_RYU = SubtraiXdeY(HP_RYU, VALOR_ATAQUE)
+    #chamo a função que retornará TRUE caso o hp seja negativo e depois atribuo 0 ao hp do ryu, outrem, nada acontece
+            if VerifyLifeNeg(HP_RYU):
                 HP_RYU=0
-            #termino printando o valor do hp de ambos os jogadores e adicionando 1 ao número de golpes
-            #realizados pelo ken
-            print(f"HP RYU = {HP_RYU}")
-            print(f"HP KEN = {HP_KEN}")
-            GOLPES_KEN += 1
-    else: #quando a condição do while for aceita, ele verificará quem foi o jogador que ganhou e
+    #chamo a função que irá printar o hp dos lutadores
+            PrintHPS(HP_RYU, HP_KEN)
+    #chamo a função que irá incrementar o valor dos golpes de ryu e já atribuir à variavel golpes
+            GOLPES_KEN = Incremento(GOLPES_KEN)
+    else: #quando a condição do while não for mais válida, ele verificará qual foi o ganhador e 
           #chamará a função da vitória de acordo com o lutador correspondente
         if HP_RYU==0:
-            vitoria_ken(GOLPES_RYU, GOLPES_KEN)
+            KenVitory(GOLPES_RYU, GOLPES_KEN)
         elif HP_KEN==0:
-            vitoria_ryu(GOLPES_RYU, GOLPES_KEN)
-
-
-
-#chamando função principal
+            RyuVitory(GOLPES_RYU, GOLPES_KEN)
+################################################
 main()
