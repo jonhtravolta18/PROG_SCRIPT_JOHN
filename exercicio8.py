261d69a4090ded25b0e4858ca762360bf2e17c24
johntravolta18 <j.sa@ufu.br>
2022-11-28 16:56:39 -0300

    teste2

--- exercicio8.py
+++ exercicio8.py
@@ -3,3 +3,11 @@
 # MATRICULA = 12211EAU020
 # TRABALHO = ps-07-plataforma
 #############################################################################################################
+n = int(input("Qntd de posições: "))
+j = 0
+while j<n:
+    plataforma = [int(i) for i in input().split( )]
+    j+=1
+i = int(input())
+
+print(plataforma)
