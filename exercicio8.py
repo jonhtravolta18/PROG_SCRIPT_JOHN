1598417f36d766d5c1815ca61d83f9bba98e5eb9
johntravolta18 <j.sa@ufu.br>
2022-11-28 17:50:47 -0300

    att

--- exercicio8.py
+++ exercicio8.py
@@ -3,21 +3,14 @@
 # MATRICULA = 12211EAU020
 # TRABALHO = ps-07-plataforma
 #############################################################################################################
-plataforma = [int(i) for i in input().split()]
-i = int(input())
+posicoes_plat = [int(i) for i in input().split()]
 
-valores = []
-valores.append(i)
+pos_inicial = int(input("Posição inicial: "))
 
-while i>=1 and i<=len(plataforma):
-    if i>0:
-        valores.append(plataforma[i + plataforma[i]])
-    elif i<0:
-        valores.append(plataforma[i - plataforma[i] * -1])  
-    else:
-        if i<1:
-            print("esquerda")
-        elif i>len(plataforma):
-            print("direita")
-        else:
-            print("loop")
+pos_passadas = [pos_inicial]
+
+j = 1
+while ():
+    
+
+
