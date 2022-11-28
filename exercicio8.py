4282a06e9587410c6fa5d3b35a2c780890e4068a
johntravolta18 <j.sa@ufu.br>
2022-11-28 17:22:59 -0300

    trab

--- exercicio8.py
+++ exercicio8.py
@@ -3,11 +3,21 @@
 # MATRICULA = 12211EAU020
 # TRABALHO = ps-07-plataforma
 #############################################################################################################
-n = int(input("Qntd de posições: "))
-j = 0
-while j<n:
-    plataforma = [int(i) for i in input().split( )]
-    j+=1
+plataforma = [int(i) for i in input().split()]
 i = int(input())
 
-print(plataforma)
+valores = []
+valores.append(i)
+
+while i>=1 and i<=len(plataforma):
+    if i>0:
+        valores.append(plataforma[i + plataforma[i]])
+    elif i<0:
+        valores.append(plataforma[i - plataforma[i] * -1])  
+    else:
+        if i<1:
+            print("esquerda")
+        elif i>len(plataforma):
+            print("direita")
+        else:
+            print("loop")
