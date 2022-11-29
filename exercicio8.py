627d342021b5d67ab121aedc357e40ca6136457d
johntravolta18 <j.sa@ufu.br>
2022-11-29 19:04:30 -0300

    te

--- exercicio8.py
+++ exercicio8.py
@@ -10,31 +10,24 @@ def main():
     pos_passadas = [pos_inicial]
     pos_atual  = pos_inicial-1
 
-    while pos_passadas[-1]>=1 and pos_passadas[-1]<=len(pos_plat):
-        if (len(pos_passadas)>=3): 
-            if pos_passadas[-1]==pos_passadas[-3]:
+    while pos_passadas[-1]>=1 and pos_passadas[-1]<=len(pos_plat):  
+        if pos_plat[pos_atual]<0:
+            if (pos_atual) - (pos_plat[pos_atual] * -1) +1 in pos_passadas:
                 break
             else:
-                if pos_plat[pos_atual]<0:
-                    pos_passadas.append( (pos_atual) - (pos_plat[pos_atual] * -1) +1)
-                    pos_atual = (pos_atual) - (pos_plat[pos_atual] * -1)
-        
-                elif pos_plat[pos_atual]>0:
-                    pos_passadas.append( (pos_atual) + (pos_plat[pos_atual]) +1)
-                    pos_atual = (pos_atual) + (pos_plat[pos_atual])
-            
-        else:
-            if pos_plat[pos_atual]<0:
                 pos_passadas.append( (pos_atual) - (pos_plat[pos_atual] * -1) +1)
                 pos_atual = (pos_atual) - (pos_plat[pos_atual] * -1)
         
-            elif pos_plat[pos_atual]>0:
+        elif (pos_plat[pos_atual]>=0):
+            if (pos_atual) + (pos_plat[pos_atual]) +1 in pos_passadas:
+                break
+            else:
                 pos_passadas.append( (pos_atual) + (pos_plat[pos_atual]) +1)
                 pos_atual = (pos_atual) + (pos_plat[pos_atual])
 
     valores_print = []
     for p in pos_passadas:
-        if p not in valores_print and p>0 and p<=len(pos_plat):
+        if p not in valores_print and p>=0 and p<=len(pos_plat):
             valores_print.append(p)
     for valor in valores_print:
         print(valor)
